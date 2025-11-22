24a627c050deb4f1d6478c71c3046ce3882c351c
Guilherme Campos <guilhermersebc@gmail.com>
2025-11-22 03:03:02 -0300

    Organização das rotas no app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 const http = require("http")
+const app = require("./conexao/server.js")
+const path = require('path');
 
-const app = require("./conexao/server")
+const userRouters = require ('./LinkSQL/Routers/userRouters.js');
+
+app.use("/users", userRouters);
 
 
+//pagina de erro
+app.use((req, res) => {
+    return res.status(404).sendFile(path.join(__dirname, '/public/erro404.html'));
+})
+
+//  crie e ativa o server
 const port = app.get('port') || 4000
-
 try {
     http.createServer(app).listen(port, () => {
         console.log(`\nServidor Rodando em http://localhost:${port}/`)
